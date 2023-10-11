c93b984fdf919df5a57324b29a776bf98e78b038
sairam <sairm21@gmail.com>
2023-10-11 21:27:16 +0530

    adding kms key id to RDS module

--- main.tf
+++ main.tf
@@ -47,18 +47,19 @@ module "rabbitmq" {
   zone_id = var.zone_id
 }
 
-/*
+
 module "rds" {
   source = "git::https://github.com/sairm21/tf-rds-module.git"
 
   for_each = var.rds
   component = each.value["component"]
-  env = var.env
   engine = each.value["engine"]
   engine_version = each.value["engine_version"]
   database_name = each.value["database_name"]
   subnet_ids = lookup(lookup(lookup(lookup(module.roboshop_VPC, "main", null), "subnet_id", null), "db", null), "subnet_id", null)
+
   tags = var.tags
+  env = var.env
+  kms_key_id = var.kms_key_id
 
 }
-*/
