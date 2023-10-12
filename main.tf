326dec9db9dabd3ac8dc4c0f7185991d525241e6
sairam <sairm21@gmail.com>
2023-10-12 22:48:24 +0530

    reverting back changes.

--- main.tf
+++ main.tf
@@ -65,6 +65,4 @@ module "rds" {
   tags = var.tags
   env = var.env
   kms_key_id = var.kms_key_id
-
-  db_subnet_group_tags = merge({ Name = "${var.env}-${each.value["component"]}-subnet-group" }, var.tags)
 }
