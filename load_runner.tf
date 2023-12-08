9aa7163c457c69e5af70089f064056ab19411118
sairam <sairm21@gmail.com>
2023-12-08 22:52:29 +0530

    adding load runner resource

---
+++ load_runner.tf
@@ -0,0 +1,25 @@
+data "aws_ami" "ami" {
+  owners = ["973714476881"]
+  most_recent = true
+}
+
+resource "aws_instance" "load_runner" {
+  ami = data.aws_ami.ami.id
+  instance_type = "t3.medium"
+  vpc_security_group_ids = ["sg-07010737da7cff878"]
+  tags = {
+    name = "load_runner"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      user = "centos"
+      password = "DevOps321"
+      host = self.public_ip
+    }
+    inline = [
+    "sudo labauto docker",
+      "sudo docker pull roboshop/rs-load:latest"
+    ]
+  }
+}
